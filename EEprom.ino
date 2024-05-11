7fcc4a8d9a622f9370eb303a1b3d90053f4a3267
lukyluk2011 <141826409+lukyluk2011@users.noreply.github.com>
2024-05-11 20:44:03 +0200

    Update EEprom.ino
    I added the function not to crash when EEprom is empty.

--- EEprom.ino
+++ EEprom.ino
@@ -1,8 +1,14 @@
 void EEprom(y, z, address) {
+try {
 x = EEPROM.read(address);
 EEPROM.update(address, y);
 if (max(x, y) - min(x, y) < z) {
 return False;
 } else {
 return True;
-}}
+} catch(Exception e) {
+
+}
+EEPROM.write(address, y);
+return True;
+}
