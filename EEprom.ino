216a75c969d657b1f2981db4ad1084fc9e781087
luckyluk.cz <141826409+lukyluk2011@users.noreply.github.com>
2024-05-12 10:00:28 +0200

    Update EEprom.ino
    I've added a program that can do both temperature and humidity in one run.

--- EEprom.ino
+++ EEprom.ino
@@ -1,14 +1,22 @@
-void EEprom(y, z, address) {
+void EEprom(t1, h1, z_t, z_h, address_t, address_h) {
 try {
-x = EEPROM.read(address);
-EEPROM.update(address, y);
-if (max(x, y) - min(x, y) < z) {
-return False;
-} else {
-return True;
-} catch(Exception e) {
-
-}
-EEPROM.write(address, y);
-return True;
+  t2 = EEPROM.read(address_t);
+  h2 = EEPROM.read(address_h)
+  EEPROM.update(address_t, t1);
+  EEPROM.update(address_h, h1)
+  if (max(t1, t2) - min(t1, t2) < z_t) {
+    a = False  
+  } else {
+    a = True
+  }
+  if (max(t1, t2) - min(t1, t2) < z_t) {
+    return False;
+  } else {
+    return True;
+  }
+catch(Exception e) {
+  EEPROM.write(address_t, t1);
+  EEPROM.write(address_h, h1)
+  return True;
+  }
 }
