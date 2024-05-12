03b1e85f58e21dd47ebe3c91c6df39a020e9750d
luckyluk.cz <141826409+lukyluk2011@users.noreply.github.com>
2024-05-12 10:07:45 +0200

    Update EEprom.ino
    missing semicolons

--- EEprom.ino
+++ EEprom.ino
@@ -1,22 +1,22 @@
 void EEprom(t1, h1, z_t, z_h, address_t, address_h) {
 try {
   t2 = EEPROM.read(address_t);
-  h2 = EEPROM.read(address_h)
+  h2 = EEPROM.read(address_h);
   EEPROM.update(address_t, t1);
-  EEPROM.update(address_h, h1)
+  EEPROM.update(address_h, h1);
   if (max(t1, t2) - min(t1, t2) < z_t) {
-    a = False  
+    a = False;
   } else {
-    a = True
+    a = True;
   }
   if (max(t1, t2) - min(t1, t2) < z_t) {
-    return False;
+    return False, a;
   } else {
-    return True;
+    return True, a;
   }
 catch(Exception e) {
   EEPROM.write(address_t, t1);
-  EEPROM.write(address_h, h1)
+  EEPROM.write(address_h, h1);
   return True;
   }
 }
